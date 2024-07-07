70c1b5ceb1a209df61655b61b5ec64bff088eca1
Loc Do <doloc181103@gmail.com>
2024-07-07 20:17:44 +0700

    feat: check for video reading permission before open file chooser

--- app/src/main/java/com/example/pe_prm392/VideoActivity.java
+++ app/src/main/java/com/example/pe_prm392/VideoActivity.java
@@ -1,26 +1,25 @@
 package com.example.pe_prm392;
 
-import android.media.MediaPlayer;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
-import android.widget.Toast;
 import android.widget.VideoView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 public class VideoActivity extends AppCompatActivity {
     VideoView videoView;
     Button btnFind;
+
+    boolean is_video_access_permitted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +27,22 @@ public class VideoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video);
         videoView = (VideoView) findViewById(R.id.vv_video);
         btnFind = (Button) findViewById(R.id.btn_find);
-        ActivityResultLauncher<String> fileChooser = registerForActivityResult(
-                new ActivityResultContracts.GetContent(),
-                new ActivityResultCallback<Uri>() {
-                    @Override
-                    public void onActivityResult(Uri o) {
-                        videoView.setVideoURI(o);
-                        videoView.start();
-                    }
-                }
-        );
+        ActivityResultLauncher<String> fileChooser = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri o) {
+                videoView.setVideoURI(o);
+                videoView.start();
+            }
+        });
 
         btnFind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fileChooser.launch("video/*");
+                if (is_video_access_permitted || ContextCompat.checkSelfPermission(VideoActivity.this, Manifest.permission.READ_MEDIA_VIDEO) == PackageManager.PERMISSION_GRANTED) {
+                    fileChooser.launch("video/*");
+                } else {
+                    requestPermissionVideoAccess();
+                }
             }
         });
 
@@ -51,7 +51,21 @@ public class VideoActivity extends AppCompatActivity {
 //        videoView.setVideoPath("https://web.law.duke.edu/cspd/contest/videos/Framed-Contest_Documentaries-and-You.mp4");
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
-
-
     }
+
+    public void requestPermissionVideoAccess() {
+        if (ContextCompat.checkSelfPermission(VideoActivity.this, Manifest.permission.READ_MEDIA_VIDEO) == PackageManager.PERMISSION_GRANTED) {
+            is_video_access_permitted = true;
+        } else {
+            request_permission_launcher_video_access.launch(Manifest.permission.READ_MEDIA_VIDEO);
+        }
+    }
+
+    private ActivityResultLauncher<String> request_permission_launcher_video_access = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+        if (isGranted) {
+            is_video_access_permitted = true;
+        } else {
+            is_video_access_permitted = false;
+        }
+    });
 }
